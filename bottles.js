74e9491356545ce4dcb8fdf8bd464f28938268ef
anika2010 <88832647+anika2010@users.noreply.github.com>
2023-01-31 20:08:20 +0530

    Add files via upload

---
+++ bottles.js
@@ -0,0 +1,46 @@
+img="";
+status="";
+objects=[];
+function preload(){
+img=loadImage("bottles.jpg");
+}
+
+function setup(){
+canvas=createCanvas(500,400);
+canvas.center();
+canvas.position(430,150);
+object_detector=ml5.objectDetector('cocossd',modalLoaded);
+document.getElementById('status').innerHTML="Status:Detecting";
+}
+function modalLoaded(){
+    console.log('Modal is loaded');
+    status=true;
+    object_detector.detect(img,gotResult);
+}
+function gotResult(error,results){
+if(error){
+    console.log(error);
+}
+console.log(results);
+objects=results;
+}
+    
+
+function draw(){ 
+    image(img,0,0,500,400);
+if(status!=''){
+    for(i=0;i<objects.length;i++){
+        document.getElementById('status').innerHTML="Status:Object Detected";
+        fill('#9a50b9');
+        noFill()
+        stroke('#9a50b9');
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y+20);
+        
+    }
+  
+}
+  
+
+}
